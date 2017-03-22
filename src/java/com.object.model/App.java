04a6696bbfb1c154dd3b38eae7144226b60d27c3
njjackzhong <njjackzhong@126.com>
2017-03-23 07:06:47 +0800

    add ObjectDB exe and demo src

--- src/java/com.object.model/App.java
+++ src/java/com.object.model/App.java
@@ -1,6 +1,7 @@
 package com.object.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by JackLee on 2017/3/22.
@@ -9,13 +10,17 @@ public class App {
     public static void main(String[] args) {
         //open a database connection
         //(create a new database if not exists)
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("points.odb");
-        EntityManager em = emf.createEntityManager();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("points.odb");        //represents a database
+        EntityManager em = emf.createEntityManager();                // represents a connection to database
+
+        //find the num of Point objects in the database
+        Query q = em.createQuery("select count(p) from Point p");
+        System.out.println("Total Points: " + q.getSingleResult());
 
         //save 1000 Point objects in the database
         em.getTransaction().begin();
-        for (int i = 0; i < 200000; i++) {
-            Point p = new Point(i,i);
+        for (int i = 100000; i < 110000; i++) {
+            Point p = new Point(i, i);
             em.persist(p);
         }
         em.getTransaction().commit();
@@ -27,8 +32,14 @@ public class App {
 
         //find the average X value
         Query q2 = em.createQuery("select avg(p.x) from Point p");
-        System.out.printf("Avg X: " + q2.getSingleResult());
+        System.out.println("Avg X: " + q2.getSingleResult());
 
+        //retrieve all the Points objects from the database
+        TypedQuery<Point> q3 = em.createQuery("select p from Point p where p.x > 100000", Point.class);
+        List<Point> results = q3.getResultList();
+
+        System.out.println("Count Point: " + results.size());
+//        results.forEach(System.out::println);
 
         //close the database connection
         em.close();
